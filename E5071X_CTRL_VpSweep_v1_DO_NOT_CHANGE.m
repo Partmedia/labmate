3848b67f4d87add842f93134de8d042bdba1c2b5
Kevin Zheng <kevinz5000@gmail.com>
2020-11-12 10:55:24 -0800

    Clean up comments and whitespace

--- E5071X_CTRL_VpSweep_v1_DO_NOT_CHANGE.m
+++ E5071X_CTRL_VpSweep_v1_DO_NOT_CHANGE.m
@@ -1,93 +1,38 @@
 % figure
-
 tic
-%% Construct visa object
-
-% Tool Name                  GPIB#
-% NG-VS-03                    1
-% NG-NA-03                    17                   
-
-% VNA = visa('ni', 'GPIB0::16::INSTR');
-% VS_03 = visa('ni','GPIB0::1::INSTR');
-% 
-% VNA.InputBufferSize = 1601*40;
-% VNA.Timeout = 600;
-% 
-% fopen(VNA); % VNA
-% fopen(VS_03); % Voltage Source
-% fprintf(VNA,':SYST:PRES');
-% 
-% %% Set trigger source to BUS
-% 
-% fprintf(VNA, ':TRIG:SOUR BUS');
-% %% Set Parameters
-% 
-% % Set VNA parameters
-% 
-% fc = 50e6; % center frequency
-% span = 10e6; % frequency span
-% num_point = 1601; % number of points
-% power = -50; % power level
-% if_bw = 1e2; % IF BW
-% sweep_type = ' LIN'; % sweep type
-% stress_chann = 1; % stimulating channel
-% sense_chann = 3; % sensing channel
-% file_name = 'test.s2p'; % saved file name
-% 
-% % Set Voltage source parameters
-% 
-% v_port = 1;
-% %v_target = 1.5;
-% v_step = 0.1;
-% v_time_step = 0.5;
-% v_tolerance = 1e-2;
-% v_lim_up = 15;
-% v_lim_btm = 0;
-% inc = true;
-% dec = ~inc;
-% 
-% % Local file save name
-% lo_file_name = 'T';
-% lo_file_number = '0';
 
 %% Set Voltage 
 Set_Voltage(VS_03, 1, v_target)
 
 %% Set Sweep Type
-
 set_swe_typ_stress = strcat(':SENS',num2str(stress_chann),':SWE:TYPE',sweep_type);
 set_swe_typ_sense = strcat(':SENS',num2str(sense_chann),':SWE:TYPE',sweep_type);
 
 %% Set Center Frequency
-
 set_center = strcat(':SENS',num2str(stress_chann),':FREQ:CENT',{' '},num2str(fc));
 set_center_stress = set_center{1};
 set_center = strcat(':SENS',num2str(sense_chann),':FREQ:CENT',{' '},num2str(fc));
 set_center_sense = set_center{1};
 
 %% Set Frequency Span
-
 set_span = strcat(':SENS',num2str(stress_chann),':FREQ:SPAN',{' '},num2str(span));
 set_span_stress = set_span{1};
 set_span = strcat(':SENS',num2str(sense_chann),':FREQ:SPAN',{' '},num2str(span));
 set_span_sense = set_span{1};
 
 %% Set Number of Points
-
 set_point = strcat(':SENS',num2str(stress_chann),':SWE:POIN',{' '},num2str(num_point));
 set_point_stress = set_point{1};
 set_point = strcat(':SENS',num2str(sense_chann),':SWE:POIN',{' '},num2str(num_point));
 set_point_sense = set_point{1};
 
 %% Set IF BW
-
 set_if = strcat(':SENS',num2str(stress_chann),':BAND',{' '},num2str(if_bw));
 set_if_stress = set_if{1};
 set_if = strcat(':SENS',num2str(sense_chann),':BAND',{' '},num2str(if_bw));
 set_if_sense = set_if{1};
 
 %% Set Power
-
 set_pw = strcat(':SOUR',num2str(stress_chann),':POW',{' '},num2str(power));
 set_pw_stress = set_pw{1};
 set_pw = strcat(':SOUR',num2str(sense_chann),':POW',{' '},num2str(power));
@@ -117,27 +62,18 @@ fprintf(VNA, strcat('CALC1:PAR1:DEF S',num2str(sense_chann),num2str(stress_chann
 fprintf(VNA, strcat('CALC1:PAR1:SEL'));
 fprintf(VNA, strcat('CALC1:FORM MLOG'));
 
-
-
-
 %% Run Measurement
-
 fprintf(VNA, 'TRIG:SING');
 
 %% Wait until measurement is done
-
 pause(0.5)
-
 fprintf(VNA, ':DISP:WIND1:TRAC1:Y:AUTO');
-
 query(VNA, '*OPC?');
 
 %% Auto Scale
-
 fprintf(VNA, ':DISP:WIND1:TRAC1:Y:AUTO');
 
 %% Save SnP
-
 fprintf(VNA, 'DISP:WIND:ACT');
 fprintf(VNA, 'MMEM:STOR:SNP:FORM RI');
 
@@ -149,17 +85,12 @@ fprintf(VNA, set_snp);
 
 
 %% Retreive data
-
 freq_out = query(VNA, ':SENS1:FREQ:DATA?');
-
 mag_out = query(VNA, ':CALC1:DATA:FDAT?');
-
 fprintf(VNA, strcat('CALC1:FORM PHAS'));
-
 phs_out =  query(VNA, ':CALC1:DATA:FDAT?');
 
 %% Format data
-
 mag_f = str2double(strsplit(mag_out,','));
 freq_f = str2double(strsplit(freq_out,','));
 phs_f = str2double(strsplit(phs_out,','));
@@ -184,9 +115,7 @@ hold on
 %% Save data
 lo_file_number_temp = 0;
 
-
 while(true)
-    
     while(true)
         num_toinsert = 4-floor(log10(lo_file_number_temp));
         if(num_toinsert == Inf)
@@ -221,6 +150,4 @@ while(true)
 end
 
 %% Closing
-
-
 toc
